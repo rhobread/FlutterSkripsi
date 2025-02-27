83d8a2a60bbc49c58fd4948a58a0f88cb2802ac3
Filemon Robert Stevenson Kessing <filemonrsk@gmail.com>
2025-02-27 13:25:33 +0700

    push testbranch

--- frontend-skripsi/lib/view/main.dart
+++ frontend-skripsi/lib/view/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/view/pickgoal.dart';
+import 'package:flutter_application_1/view/signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: const PickGoalPage(
-          userId: '1',
-        ));
+        debugShowCheckedModeBanner: false, home: const SignUpPage());
   }
 }
